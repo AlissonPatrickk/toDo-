1a948494fe4ea104153d211ee51a707a241e0456
alisson.souza <alisson.souza@hitechnologies.com.br>
2021-12-13 16:03:21 -0300

    toDo

---
+++ src/views/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container mt-2">
+    <div v-for="(task, index) in tasks" :key="index">
+    <b-card :title="task.subject" class="mb-2">
+        <b-card-text>{{task.description}}</b-card-text>
+        <b-button variant="outline-secondary" class="mr-2" @click="edit(task, index)">Editar</b-button>
+        <b-button variant="outline-danger" class="mr-2" @click="remove(index)">Excluir</b-button>
+    </b-card>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name:"List",
+    data() {
+        return{
+            tasks: [],
+        }
+    },
+    created(){
+        this.tasks = (localStorage.getItem("tasks")) ? JSON.parse(localStorage.getItem("tasks")) : [];
+    },
+    methods:{
+        edit(index){
+            this.$router.push({ name: "form", params: {index}})
+        }
+    },
+}
+</script>
