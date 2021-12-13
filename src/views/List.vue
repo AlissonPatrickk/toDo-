5cbab765f88b991af99ca2c08258beb97a48cef4
alisson.souza <alisson.souza@hitechnologies.com.br>
2021-12-13 16:17:05 -0300

    quase

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,29 +1,60 @@
 <template>
   <div class="container mt-2">
     <div v-for="(task, index) in tasks" :key="index">
-    <b-card :title="task.subject" class="mb-2">
-        <b-card-text>{{task.description}}</b-card-text>
-        <b-button variant="outline-secondary" class="mr-2" @click="edit(task, index)">Editar</b-button>
-        <b-button variant="outline-danger" class="mr-2" @click="remove(index)">Excluir</b-button>
-    </b-card>
+      <b-card :title="task.subject" class="mb-2">
+        <b-card-text>{{ task.description }}</b-card-text>
+        <b-button variant="outline-secondary" class="mr-2" @click="edit(index)"
+          >Editar</b-button
+        >
+        <b-button
+          variant="outline-danger"
+          class="mr-2"
+          @click="remove(task, index)"
+          >Excluir</b-button
+        >
+      </b-card>
     </div>
+    <b-modal ref="modalRemove" hide-footer title="Exclusão de tarefa">
+      <div class="d-block text-center">
+        Deseja realmente Excluir essa tarefa?
+      </div>
+      <div class="mt-3 d-flex justify-content-end">
+        <b-button variant="outline-secondary" class="mr-2" @click="hideModal"
+          >cancelar</b-button
+        >
+        <b-button
+          variant="outline-danger"
+          class="mr-2"
+          @click="confirmRemoveTask"
+          >Excluir</b-button
+        >
+      </div>
+    </b-modal>
   </div>
 </template>
 <script>
 export default {
-    name:"List",
-    data() {
-        return{
-            tasks: [],
-        }
+  name: "List",
+  data() {
+    return {
+      tasks: [],
+      taskSelected: [],
+    };
+  },
+  created() {
+    this.tasks = localStorage.getItem("tasks")
+      ? JSON.parse(localStorage.getItem("tasks"))
+      : [];
+  },
+  methods: {
+    edit(index) {
+      this.$router.push({ name: "form", params: { index } });
     },
-    created(){
-        this.tasks = (localStorage.getItem("tasks")) ? JSON.parse(localStorage.getItem("tasks")) : [];
+    remove(task, index) {
+      this.taskSelected = task;
+      this.taskSelected.index = index;
+      this.$refs.modalRemove.show();
     },
-    methods:{
-        edit(index){
-            this.$router.push({ name: "form", params: {index}})
-        }
-    },
-}
+  },
+};
 </script>
